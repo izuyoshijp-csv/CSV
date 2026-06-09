7cfb002d5564c5d9ec6405451f7c43751f83f342
thuhatran1508-dot <thuhatran1508@gmail.com>
2026-06-09 14:18:28 +0700

    commit

--- src/app/(private)/csv-create/page.tsx
+++
@@ -1,5 +0,0 @@
-import { CsvCreatePageContent } from "@/modules/csv-create/components/csv-create-page-content"
-
-export default function CsvCreatePage() {
-  return <CsvCreatePageContent />
-}
--- src/app/(private)/settings/page.tsx
+++
@@ -1,5 +0,0 @@
-import { MappingPageContent } from "@/modules/import-mapping/components/mapping-page-content"
-
-export default function SettingsPage() {
-  return <MappingPageContent />
-}
